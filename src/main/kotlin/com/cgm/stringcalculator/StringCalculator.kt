761797049643b0ceed6865d1d98a9c454116fbb7
Silvana Bonicelli <silvana.bonicelli@cgm.com>
2021-04-19 11:35:44 +0200

    KATA String Calculator: step 4

--- src/main/kotlin/com/cgm/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/com/cgm/stringcalculator/StringCalculator.kt
@@ -2,10 +2,28 @@ package com.cgm.stringcalculator
 
 class StringCalculator {
     fun add(numbers: String): Int {
-        val charToSplit = ","
+        var charToSplit = ","
+        var listOfNumbers = numbers
+
+        if (numbers.startsWith("//")){
+            charToSplit = numbers.substring(2,3)
+            listOfNumbers = numbers.substring(4,numbers.length)
+        }
+
         return if (numbers.isNullOrEmpty()) 0
-               else numbers.replace("\n",charToSplit).split(charToSplit).sumBy{it.toInt()}
+               else listOfNumbers.replace("\n",charToSplit).split(charToSplit).sumBy{it.toInt()}
     }
+
+//    fun add(numbers: String): Int {
+//        var charToSplit = ","
+//        var listOfNumbers = numbers
+//
+//        if (numbers.startsWith("//")){
+//            charToSplit = numbers.substring(2,3)
+//            listOfNumbers = numbers.substring(4,numbers.length)
+//        }
+//
+//        return if (numbers.isNullOrEmpty()) 0
+//        else listOfNumbers.replace("\n",charToSplit).split(charToSplit).sumBy{it.toInt()}
+//    }
 }
-
-
--- src/test/kotlin/com/cgm/stringcalculator/MainTest.kt
+++ src/test/kotlin/com/cgm/stringcalculator/MainTest.kt
@@ -2,7 +2,6 @@ package com.cgm.stringcalculator
 
 import org.junit.jupiter.api.Test
 import org.junit.Assert
-import org.junit.jupiter.api.assertThrows
 
 class MainTest {
     @Test
@@ -25,6 +24,21 @@ class MainTest {
     @Test
     fun add_numbers_with_newLine_ReturnSumOfNumbers() {
         Assert.assertEquals(10,StringCalculator().add("1\n2,3,4"))
+    }
+
+    @Test
+    fun add_numbers_with_special_separator_ReturnSumOfNumbers() {
+        Assert.assertEquals(6,StringCalculator().add("//;\n1;2;3"))
+    }
+
+    @Test
+    fun add_numbers_with_special_separator_and_new_line_ReturnSumOfNumbers() {
+        Assert.assertEquals(6,StringCalculator().add("//;\n1\n2;3"))
+    }
+
+    @Test
+    fun add_with_special_separator_two_numbers_ReturnSumOfTwoNumbers() {
+        Assert.assertEquals(3,StringCalculator().add("//;\n1;2"))
     }
 
 //    @Test
